f1d453d3c6f5dd90473a81d3fbee20e5e410d0bd
BCIs-maniac81 <bendibhafededdine@gmail.com>
2022-09-15 17:59:19 +0100

    adding source files

---
+++ script.js
@@ -0,0 +1,11 @@
+/* function first() {
+    var greet = 'This is a funny message !!!!';
+    function second() {
+        alert(greet);
+    }
+    return second;
+}
+
+var newfunc = first();
+newfunc(); */
+
